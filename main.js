c408a662abded7f2eacff7264bc90b1b5dd11add
VladKhrlmv <kharlamov-vlad03@mail.ru>
2019-11-12 22:35:17 +0300

    changes in the main page

---
+++ main.js
@@ -0,0 +1,8 @@
+var header = document.querySelector('.header');
+var text = document.querySelector('.t');
+window.addEventListener('scroll', function() {
+    header.style.opacity = 1 - +window.pageYOffset/700+''; 
+    header.style.top = +window.pageYOffset + 'px'; 
+    header.style.backgroundPositionY = - +window.pageYOffset / 2 + 'px'; 
+    
+});
