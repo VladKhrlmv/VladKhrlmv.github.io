057107bd4a2a16421679ea4c95dd523272196e0f
VladKhrlmv <kharlamov-vlad03@mail.ru>
2020-03-26 15:16:55 +0300

    new:ripple-effect

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ window.addEventListener('scroll', function() {
     header.style.opacity = 1 - +window.pageYOffset/700+''; 
     header.style.top = +window.pageYOffset + 'px'; 
     header.style.backgroundPositionY = - +window.pageYOffset / 2 + 'px';
-});
+}); 
